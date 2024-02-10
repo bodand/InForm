0b7a4d9d45ad32b4e883326b203c3eee12afdf43
bodand <bodand@proton.me>
2024-02-10 19:45:29 +0100

    feat(db): added fill data to form elements
    - form elemnts' filled data can now be stored
    - each fill type is stored in their own table, allowing the generation of smaller tables, and always well-typed connections in C# code

--- InForm.Server/Features/Forms/Db/_Context.cs
+++ InForm.Server/Features/Forms/Db/_Context.cs
@@ -12,12 +12,24 @@ public partial class InFormDbContext
             .HasMany(x => x.FormElementBases)
             .WithOne(x => x.ParentForm)
             .OnDelete(DeleteBehavior.Cascade);
+
         modelBuilder.Entity<FormElementBase>()
             .UseTphMappingStrategy();
+        modelBuilder.Entity<FillData>()
+            .UseTpcMappingStrategy();
+
+        modelBuilder.Entity<StringFormElement>()
+            .HasMany(x => x.FillData)
+            .WithOne(x => x.ParentElement)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 
 #nullable disable
     public DbSet<Form> Forms { get; set; }
+    
     public DbSet<FormElementBase> FormElementBases { get; set; }
+    public DbSet<FillData> FillData { get; set; }
+
     public DbSet<StringFormElement> StringFormElements { get; set; }
+    public DbSet<StringFillData> StringFillData { get; set; }
 }
---
+++ InForm.Server/Migrations/20240210184210_FillData.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InForm.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FillData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "FillDataSequence",
+                schema: "InForm");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxLength",
+                schema: "InForm",
+                table: "FormElementBases",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "StringFillData",
+                schema: "InForm",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false, defaultValueSql: "nextval('\"InForm\".\"FillDataSequence\"')"),
+                    ParentElementId = table.Column<long>(type: "bigint", nullable: true),
+                    Value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StringFillData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StringFillData_FormElementBases_ParentElementId",
+                        column: x => x.ParentElementId,
+                        principalSchema: "InForm",
+                        principalTable: "FormElementBases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StringFillData_ParentElementId",
+                schema: "InForm",
+                table: "StringFillData",
+                column: "ParentElementId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StringFillData",
+                schema: "InForm");
+
+            migrationBuilder.DropSequence(
+                name: "FillDataSequence",
+                schema: "InForm");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "MaxLength",
+                schema: "InForm",
+                table: "FormElementBases",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
