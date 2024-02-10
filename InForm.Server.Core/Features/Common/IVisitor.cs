980e2b4461abd908f172e0abaa44a32cfe7b9b1e
bodand <bodand@proton.me>
2024-02-10 21:17:47 +0100

    chore: added compile checks to visitor
    - visitor can now only visit vistables types

--- InForm.Server.Core/Features/Common/IVisitor.cs
+++ InForm.Server.Core/Features/Common/IVisitor.cs
@@ -9,11 +9,13 @@ public interface IVisitor
 }
 
 public interface ITypedVisitor<in TVisited, out TResult> : IVisitor<TResult>
+    where TVisited : IVisitable
 {
     TResult Visit(TVisited visited);
 }
 
 public interface ITypedVisitor<in TVisited> : IVisitor
+    where TVisited : IVisitable
 {
     void Visit(TVisited visited);
 }
---
+++ InForm.Server.Core/Features/Forms/Fill.cs
@@ -0,0 +1,54 @@
+﻿using InForm.Server.Core.Features.Common;
+
+namespace InForm.Server.Core.Features.Forms;
+
+/// <summary>
+///     A request DTO for creating a fill for a form.
+///     The form's validation will be executed on the data on the server.
+/// </summary>
+/// <param name="FormId">The identifier of the form.</param>
+/// <param name="Elements">The list of form elements' fills to insert.</param>
+public readonly record struct FillRequest(
+    Guid FormId,
+    List<FillElement> Elements
+);
+
+/// <summary>
+///     Abstract base of typed fill elements.
+/// </summary>
+/// <param name="Id">The identifier of the form element this fill element is for.</param>
+public abstract record FillElement(
+    long Id
+) : IVisitable
+{
+    /// <inheritdoc/>
+    public abstract void Accept(IVisitor visitor);
+
+    /// <inheritdoc/>
+    public abstract TResult? Accept<TResult>(IVisitor<TResult> visitor) where TResult : notnull;
+}
+
+/// <summary>
+///     The fill element for string fill elements.
+/// </summary>
+/// <param name="Id">The identifier of the string form element.</param>
+/// <param name="Value">The value of the element.</param>
+public record StringFillElement(
+    long Id,
+    string? Value
+) : FillElement(Id)
+{
+    /// <inheritdoc/>
+    public override void Accept(IVisitor visitor)
+    {
+        if (visitor is not ITypedVisitor<StringFillElement> typed) return;
+        typed.Visit(this);
+    }
+
+    /// <inheritdoc/>
+    public override TResult? Accept<TResult>(IVisitor<TResult> visitor) where TResult : default
+    {
+        if (visitor is not ITypedVisitor<StringFillElement, TResult> typed) return default;
+        return typed.Visit(this);
+    }
+}
